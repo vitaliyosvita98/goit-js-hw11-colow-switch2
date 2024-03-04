4263e52837b88bb33eba18bd55cbec57bfc77e1e
vitaliyosvita98 <40260664+vitaliyosvita98@users.noreply.github.com>
2024-03-04 19:40:58 +0200

    Add files via upload

---
+++ js/array.js
@@ -0,0 +1,7 @@
+const managers = ["Petro", "Viktor", "Anna", "Olga"];
+const sales = [20000, 34000, 17000, 23000];
+
+for(let i=0; i < managers.length; i++) {
+    console.log(`${managers[i]} - ${sales[i]}`);
+}
+
